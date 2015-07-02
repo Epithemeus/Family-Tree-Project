f49a4f7e05911fc93ab67faa7213bf98cb8b9f33
Epithemeus <banemalar7@hotmail.com>
2015-07-02 14:40:13 -0700

    Update FamilyTree.py

--- FamilyTree.py
+++ FamilyTree.py
@@ -1,54 +1,13 @@
-import csv
+from Person import *
+from TreeReader import *
+from TraversalFunctions import *
 
-class Person(object):
-	def __init__(self, name, mothersName, fathersName):
-		self.mothersName = mothersName
-		self.fathersName = fathersName
-		self.name = name
-		self.father = None
-		self.mother = None
-		self.children = []
-		
-	def __str__(self):
-		return self.name
-
-	def checkLowerGen(self, person, generations):
-                generations = generations + 1
-                if self.name = person.name:
-                        return true
-                for child in self.children:
-                        child.checkLowerGen(person, generations)
-
-familyTreeFile = open('RoyalFamily.csv', 'r')
-
-reader = csv.reader(familyTreeFile, delimiter=',', quotechar='|')
-familyTree = []
-
-#This needs to be fixed
-for individual in reader:
-	familyTree.append(Person(individual[0], individual[2], individual[1]))
+treeReader = TreeReader('RoyalFamily.csv')
+familyTree = treeReader.create_tree()
 
 for individual in familyTree:
-	for parent in familyTree:
-		if individual.fathersName == parent.name:
-			individual.father = parent
-		elif individual.mothersName == parent.name:
-			individual.mother = parent
-
-for individual in familyTree:
-	if individual.father:
-		individual.father.children.append(individual)
-	if individual.mother:
-		individual.mother.children.append(individual)
-
-#get names here, dummy for now
-name1 = 'William Windsor'
-name2 = 'Peter Phillips'
-
-for individual in familyTree:
-        if individual.name = name1:
-                name1 = individual
-        if individual.name = name2:
-                name2 = individual
-
-    
+    if individual.children:
+        print(individual.name + '\'s children are ', end='')
+        for child in individual.children:
+            print(child.name, end=', ')
+        print()
