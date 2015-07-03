88b8b478697d259a35cbf10e7a20a79318c996c7
Epithemeus <banemalar7@hotmail.com>
2015-07-03 11:21:05 -0700

    Update FamilyTree.py

--- FamilyTree.py
+++ FamilyTree.py
@@ -5,9 +5,29 @@ from TraversalFunctions import *
 treeReader = TreeReader('RoyalFamily.csv')
 familyTree = treeReader.create_tree()
 
+#This is to confirm traversal functions functioning
+generations = [0, 0]
+personName = input('Enter a name\n')
+targetName = input('Enter another name\n')
+person = None
+target = None
 for individual in familyTree:
-    if individual.children:
-        print(individual.name + '\'s children are ', end='')
-        for child in individual.children:
-            print(child.name, end=', ')
-        print()
+    if individual.name == personName:
+        person = individual
+    if individual.name == targetName:
+        target = individual
+
+##if person and target:
+##    if traverse_down_tree(person, target, generations):
+##        print('There are ' + str(generations[0]) + ' generations between them')
+##    elif traverse_down_tree(target, person, generations):
+##        print('There are ' + str(generations[0]) + ' generations between them')
+##    else:
+##        print('This relationship is not parental')
+
+if person and target:
+    if traverse_up_tree(person, target, generations):
+        print('Generations down ' + str(generations[0]))
+        print('Generations up ' + str(generations[1]))
+    else:
+        print('Weird')
